51fe2e4530f0aad41b4a26cd048742ce0c731e23
andor <andor2001@ukr.net>
2024-09-18 03:38:06 +0300

    test aws tf

---
+++ TF/test_aws.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 5.0" #version
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+  profile = "terraform"
+}
+
+resource "aws_instance" "web-srv" {
+  ami = "ami-03cc8375791cb8bcf"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "web-test-terraform-srv"
+  }
+}
